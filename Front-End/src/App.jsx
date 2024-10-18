3a79d9d8020fb5e6e866f492eb813cf4573543a5
XAnthekiraX <alexbalverde123@gmail.com>
2024-10-17 23:23:56 -0500

    Configuraciones y preubas

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -9,13 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hola</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className='border-red-500 bg-inherit from-inherit'>Hola</h1>
+        <div className='flex items-center justify-center '>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
--- Front-End/tailwind.config.js
+++ Front-End/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,cjs,mjs,ts,cts,mt}",
   ],
   theme: {
     extend: {},
